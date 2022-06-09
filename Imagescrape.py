634cc6ddf8db3eed091db857ae605dad63e38614
Renzil DSouza <renzil@gmail.com>
2022-06-09 11:39:23 +0000

    Read thumbnails and upload to imagekit

--- Imagescrape.py
+++ Imagescrape.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 import pandas as pd
 import csv
 from bs4 import BeautifulSoup
@@ -5,6 +7,22 @@ import requests
 import urllib.parse
 from urllib.parse import quote
 import re
+from imagekitio import ImageKit
+
+imagekit = None
+
+load_dotenv()
+
+private_key = os.getenv("PRIVATE_KEY")
+public_key = os.getenv("PUBLIC_KEY")
+url_endpoint = os.getenv("URL_ENDPOINT")
+
+if private_key and public_key and url_endpoint:
+    imagekit = ImageKit(
+        private_key=private_key,
+        public_key=public_key,
+        url_endpoint=url_endpoint
+    )
 
 #open the website
 
@@ -24,10 +42,13 @@ df= pd.DataFrame(data,columns=['Title'])
 
 
 df = df.reset_index() # make sure indexes pair with number of rows
-header = ['Title', 'Author', 'Description']
+header = ['Title', 'Author', 'Description', 'Image URL']
 
-def get_book_metadata(parent, title):
+def get_book_metadata(soup, parent, title):
     booklist=[]
+    image_base64_scripts = soup.find_all("script", string=re.compile(re.escape("(function(){var s='data")))
+    image_base64s = [re.sub(re.compile("\\'\\;var\\ ii\\=\\[\\'dimg_\d\d?\\'\\]\\;\\_setImagesSrc\\(ii\\,s\\)\\;\\}\\)\\(\\)\\;"), "", x.string.replace("(function(){var s='", "")) for x in image_base64_scripts]
+
     children = parent.findChildren(recursive=False)
     for child in children:
         authorname = ""
@@ -45,6 +66,7 @@ def get_book_metadata(parent, title):
         #print(description[0].text)
         if description: 
             descriptiontext = description[0].text
+
         booklist.append({
             "author": authorname,
             "description": descriptiontext
@@ -56,20 +78,39 @@ def get_book_metadata(parent, title):
     print("The list is complete..Please choose your option.Input 11 to skip", list(bookrange))
     userchoicedesc= int(input("Enter your choice"))
     if(userchoicedesc==11):
-        data = [title, "None", "None"]
-        reader=csv.reader(data,delimiter=':', quoting=csv.QUOTE_NONE)
+        data = [title, "None", "None", "None"]
+        reader=csv.reader(data, quoting=csv.QUOTE_NONE)
         return reader
 
     if(userchoicedesc in bookrange):
         print(booklist[userchoicedesc-1])
-    
-    
-
     else:
         print("Out of range")
 # converting into CSV files
-    data = [title, booklist[userchoicedesc-1]["author"], booklist[userchoicedesc-1]["description"]]
-    reader=csv.reader(data,delimiter=':', quoting=csv.QUOTE_NONE)
+
+    image_base64 = None
+    if userchoicedesc < len(image_base64s):
+        image_base64 = image_base64s[userchoicedesc]
+    
+    image_url = "None"
+    if image_base64 and imagekit:
+        image_upload_result = imagekit.upload_file(
+            file=image_base64, # required
+            file_name=title, # required
+            options= {
+                "folder" : "/LOBH/book-thumbs/",
+                "tags": ["lobh-book-thumb"],
+                "is_private_file": False,
+                "use_unique_file_name": True,
+                "response_fields": ["is_private_file", "tags"],
+            }
+        )
+        print(image_upload_result)
+        image_url = image_upload_result["response"]["url"]
+        print(image_url)
+        # image_base64_bin = base64.b64decode(image_base64)
+    data = [title, booklist[userchoicedesc-1]["author"], booklist[userchoicedesc-1]["description"], image_url]
+    reader=csv.reader(data, quoting=csv.QUOTE_NONE)
     return reader
 
 with open('data.csv', 'w', encoding='UTF8', newline='') as csv_file:
@@ -112,7 +153,7 @@ with open('data.csv', 'w', encoding='UTF8', newline='') as csv_file:
         print(text_encoded)
         text_encoded = text_encoded.replace(" ", "%20")
         parent = soup.find("div",attrs={"data-async-context": "query:"+text_encoded})
-        book_metadata=get_book_metadata(parent, title)
+        book_metadata=get_book_metadata(soup, parent, title)
         if(book_metadata!= None):
             writer.writerow(book_metadata)
             csv_file.flush()
